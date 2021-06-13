3180b53beeb7625262cfae573cbd72ce94d3a5af
111989 <6uptaaashi@gmail.com>
2021-06-12 20:02:52 -0400

    Update cartpole_q_learning.py

--- scripts/cartpole_q_learning.py
+++ scripts/cartpole_q_learning.py
@@ -3,8 +3,7 @@
     actions (left & right), and CartPole 
     has discrete action space. The 'epsilon' 
     value is used to balance 'Exploration' 
-    vs 'Exploitation.'"""
-
+    vs 'Exploitation'"""
 
 import gym
 import argparse
@@ -12,14 +11,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from IPython import display as i_python_display
 
-
 class MyEnvironment:
     def __init__(self, environment_name: str, action = None, observation = None) -> None:
         self.environment_name = environment_name
         self.environment = gym.make(self.environment_name)
         self.action = action
         self.observation = observation
-
         if 'CartPole' in self.environment_name:
             self.n_bins = 20
             self.bins = [np.linspace(-4.8, 4.8, self.n_bins), \
@@ -38,9 +35,7 @@ class MyEnvironment:
     def get_action_space_length(self):
         if 'CartPole' in self.environment_name:
             return 2
-
         return self.environment.action_space.n
-
 
     def get_observation_space(self):
         return self.environment.observation_space
@@ -49,9 +44,7 @@ class MyEnvironment:
         if 'CartPole' in self.environment_name:
             return [self.n_bins+1] \
                 * len(self.environment.observation_space.high)
-
         return [self.environment.horizon + 1]
-
 
     def set_observation(self, observation) -> None:
         self.observation = observation
@@ -61,9 +54,7 @@ class MyEnvironment:
             for i in range(len(self.get_observation_space().high)):
                 observation_index.append(np.digitize(self.observation[i], \
                     self.bins[i]) - 1)  # subtract 1 to convert bin into index 
-
             self.observation = tuple(observation_index)
-
 
     def display_environment(self):
         plt.imshow(self.environment.render(mode = 'rgb_array'))
@@ -93,13 +84,10 @@ class Agent:
         self.observation_space_length = observation_space_length 
         self.n_episodes = n_episodes 
         self.epsilon = np.exp(-5 * np.linspace(0, 1, n_episodes)) 
-
         self.q_table = np.random.uniform(low = -2, high = 0, \
             size = (self.observation_space_length + [self.action_space_length]))
-
         self.learning_rate = learning_rate
         self.gamma = gamma 
-
     
     def act(self, observation: int, episode_number: int) -> int:
         """
@@ -109,13 +97,11 @@ class Agent:
             the action space if the random number 
             generated is less than epsilon, else, 
             the action that has the highest 
-            Q-value in observation/ state."""
+            Q-value in observation/ state"""
 
         if np.random.uniform(1.0, 0.0) < self.epsilon[episode_number]: 
             return self.action_space.sample()
-
         return np.argmax(self.q_table[observation]) 
-
 
     def update(self, observation: int, action: int, \
         next_observation: int, reward: float) -> float:
@@ -123,15 +109,13 @@ class Agent:
             Estimates the optimal future Q-value
             i.e., the maximum Q-value in next
             observation, updates the Q-table and
-            returns the change in Q-value."""
+            returns the change in Q-value"""
 
         q_value = self.q_table[observation][action]
         next_q_value_estimate = np.max(self.q_table[next_observation]) 
         temporal_difference_target = reward + self.gamma * next_q_value_estimate
-
         self.q_table[observation][action] \
             += self.learning_rate * (temporal_difference_target - q_value)
-        
         return self.q_table[observation][action] - q_value
 
 
@@ -152,6 +136,7 @@ def plot_statistics(running_accuracy, running_delta):
 
 
 def main():
+    
     np.random.seed(111989)
     
     # display once in every 100 episodes 
@@ -177,17 +162,17 @@ def main():
     # initialize performance indicators
     running_length = 5 
     running_delta, running_accuracy = [], [] 
-
-
-
     render_gym = True
+    
+    
+    
     for episode_index in range(n_episodes):
         """
             Track the number of episodes completed
             (episode_count), the number of actions 
             that yield maximum reward or successes
             (optimal_actions), cumulative reward 
-            and delta update of the Q-table."""
+            and delta update of the Q-table"""
 
         episode_count = 1 
         optimal_actions = 0  
@@ -198,8 +183,6 @@ def main():
         environment.set_observation(observation)
         observation = environment.observation 
 
-
-
         done = False
         while not done:
             # display performance
@@ -209,8 +192,8 @@ def main():
                     i_python_display.clear_output(wait = True)
                     i_python_display.display(plt.gcf())
 
-
-
+                    
+                    
             action = agent.act(observation, episode_index) 
             environment.action = action
             next_observation, reward, done, _ = environment.step()
@@ -222,12 +205,10 @@ def main():
 
             observation = next_observation 
 
-        
-
+            
+            
             optimal_actions += int(reward > 0.0)
             episode_count += 1
-
-
 
         # compute performance statistics and display performance
         running_delta.append(sum(delta_update).item()) # latest update
@@ -242,8 +223,6 @@ def main():
                 all([delta < 0.0001 for delta in running_delta]) and \
                     episode_index >= running_length): break
 
-
-
     i_python_display.clear_output(wait = True)
     environment.close() 
 
@@ -251,7 +230,6 @@ def main():
     plot_statistics(running_accuracy, running_delta)
 
 
-    
 
 if __name__ == "__main__":
     
@@ -268,6 +246,5 @@ if __name__ == "__main__":
     n_episodes = int(args['episodes'])
     learning_rate = float(args['alpha'])
     gamma = float(args['gamma'])
-
 
     main()
